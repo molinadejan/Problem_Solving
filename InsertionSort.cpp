3e9300118385c2e351136190bc7f0063cf14d32a
molinadejan <75786351+molinadejan@users.noreply.github.com>
2021-04-21 09:42:41 +0900

    Add files via upload

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -11,7 +11,21 @@ struct ListNode
 	int val;
 	ListNode* nextNode;
 	ListNode(int x, ListNode* next) : val(x), nextNode(next) {}
+	~ListNode() {std::cout << val << " delete!\n"; }
 };
+
+void PrintList(ListNode* start)
+{
+	ListNode* iter = start;
+
+	while (iter != NULL)
+	{
+		std::cout << iter->val << " ";
+		iter = iter->nextNode;
+	}
+
+	std::cout << std::endl;
+}
 
 ListNode* InsertionSort(ListNode* head)
 {
@@ -21,10 +35,13 @@ ListNode* InsertionSort(ListNode* head)
 	{
 		if (key->val > key->nextNode->val)
 		{
-			tmp = key->nextNode; // tmp = 7
-			key->nextNode = key->nextNode->nextNode; // 10 -> 6
-			tmp->nextNode = head; // 7 -> 2
-			head = key = tmp; //
+			tmp = key->nextNode;
+			key->nextNode = key->nextNode->nextNode;
+			tmp->nextNode = head;
+			head = key = tmp;
+
+			PrintList(head);
+
 			continue;
 		}
 
@@ -36,19 +53,19 @@ ListNode* InsertionSort(ListNode* head)
 
 int main(void)
 {
-	ListNode n0(20, NULL);
-	ListNode n1(6, &n0);
-	ListNode n2(7, &n1);
-	ListNode n3(10, &n2);
-	ListNode head(2, &n3);
+	ListNode* n0 = new ListNode(20, NULL);
+	ListNode* n1 = new ListNode(6, n0);
+	ListNode* n2 = new ListNode(7, n1);
+	ListNode* n3 = new ListNode(10, n2);
+	ListNode* n4 = new ListNode(2, n3);
 
-	ListNode* iter = InsertionSort(&head);
+	InsertionSort(n4);
 
-	while (iter != NULL)
-	{
-		std::cout << iter->val << std::endl;
-		iter = iter->nextNode;
-	}
+	delete n0;
+	delete n1;
+	delete n2;
+	delete n3;
+	delete n4;
 
 	return 0;
 }
