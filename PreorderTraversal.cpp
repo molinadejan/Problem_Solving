55888e94d0b5e363bedd5e9a407d6d2b38ee054d
molinadejan <75786351+molinadejan@users.noreply.github.com>
2021-04-21 13:51:00 +0900

    Add files via upload

---
+++ PreorderTraversal.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stack>
+
+struct TreeNode
+{
+	int val;
+	TreeNode* leftNode;
+	TreeNode* rightNode;
+
+	TreeNode(int x, TreeNode* left = nullptr, TreeNode* right = nullptr)
+		: val(x), leftNode(left), rightNode(right) { }
+};
+
+void PreorderTraversal(TreeNode* root)
+{
+	if (root == nullptr)
+		return;
+
+	std::cout << root->val << " ";
+
+	PreorderTraversal(root->leftNode);
+	PreorderTraversal(root->rightNode);
+}
+
+void PreorderTraversalStack(TreeNode* root)
+{
+	std::stack<TreeNode*> s;
+	s.push(root);
+
+	while (!s.empty())
+	{
+		TreeNode* curNode = s.top();
+		s.pop();
+
+		std::cout << curNode->val << " ";
+
+		if (curNode->leftNode != nullptr)
+			s.push(curNode->rightNode);
+
+		if (curNode->rightNode != nullptr)
+			s.push(curNode->leftNode);
+	}
+}
+
+int main(void)
+{
+	TreeNode* node0 = new TreeNode(18);
+	TreeNode* node1 = new TreeNode(11);
+	TreeNode* node2 = new TreeNode(8);
+	TreeNode* node3 = new TreeNode(2);
+
+	TreeNode* node4 = new TreeNode(16, node1, node0);
+	TreeNode* node5 = new TreeNode(7, node3, node2);
+
+	TreeNode* root = new TreeNode(10, node5, node4);
+
+	PreorderTraversal(root);
+
+	std::cout << "\n";
+
+	PreorderTraversalStack(root);
+
+	return 0;
+}
